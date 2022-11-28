aa03c636816e9ace744f88a416ea169cf04ea3af
Fjolnirr <yusufcan2617@gmail.com>
2022-11-28 11:18:55 +0300

    Add project files.

---
+++ RecapProject1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace RecapProject1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxCategory = new System.Windows.Forms.GroupBox();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.cbxCategory = new System.Windows.Forms.ComboBox();
+            this.gbxSearch = new System.Windows.Forms.GroupBox();
+            this.tbxSearch = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.dgwProduct = new System.Windows.Forms.DataGridView();
+            this.gbxCategory.SuspendLayout();
+            this.gbxSearch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbxCategory
+            // 
+            this.gbxCategory.Controls.Add(this.lblCategory);
+            this.gbxCategory.Controls.Add(this.cbxCategory);
+            this.gbxCategory.Location = new System.Drawing.Point(12, 12);
+            this.gbxCategory.Name = "gbxCategory";
+            this.gbxCategory.Size = new System.Drawing.Size(776, 78);
+            this.gbxCategory.TabIndex = 0;
+            this.gbxCategory.TabStop = false;
+            this.gbxCategory.Text = "List by Category";
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(30, 54);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(49, 13);
+            this.lblCategory.TabIndex = 1;
+            this.lblCategory.Text = "Category";
+            // 
+            // cbxCategory
+            // 
+            this.cbxCategory.FormattingEnabled = true;
+            this.cbxCategory.Location = new System.Drawing.Point(87, 51);
+            this.cbxCategory.Name = "cbxCategory";
+            this.cbxCategory.Size = new System.Drawing.Size(194, 21);
+            this.cbxCategory.TabIndex = 0;
+            this.cbxCategory.SelectedIndexChanged += new System.EventHandler(this.cbxCategory_SelectedIndexChanged);
+            // 
+            // gbxSearch
+            // 
+            this.gbxSearch.Controls.Add(this.tbxSearch);
+            this.gbxSearch.Controls.Add(this.lblSearch);
+            this.gbxSearch.Location = new System.Drawing.Point(12, 96);
+            this.gbxSearch.Name = "gbxSearch";
+            this.gbxSearch.Size = new System.Drawing.Size(776, 77);
+            this.gbxSearch.TabIndex = 2;
+            this.gbxSearch.TabStop = false;
+            this.gbxSearch.Text = "Search by Product Name";
+            // 
+            // tbxSearch
+            // 
+            this.tbxSearch.Location = new System.Drawing.Point(87, 45);
+            this.tbxSearch.Name = "tbxSearch";
+            this.tbxSearch.Size = new System.Drawing.Size(194, 20);
+            this.tbxSearch.TabIndex = 1;
+            this.tbxSearch.TextChanged += new System.EventHandler(this.tbxSearch_TextChanged);
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(30, 48);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(41, 13);
+            this.lblSearch.TabIndex = 0;
+            this.lblSearch.Text = "Search";
+            // 
+            // dgwProduct
+            // 
+            this.dgwProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwProduct.Location = new System.Drawing.Point(12, 192);
+            this.dgwProduct.Name = "dgwProduct";
+            this.dgwProduct.Size = new System.Drawing.Size(776, 165);
+            this.dgwProduct.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgwProduct);
+            this.Controls.Add(this.gbxSearch);
+            this.Controls.Add(this.gbxCategory);
+            this.Name = "Form1";
+            this.Text = "Products";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbxCategory.ResumeLayout(false);
+            this.gbxCategory.PerformLayout();
+            this.gbxSearch.ResumeLayout(false);
+            this.gbxSearch.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProduct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxCategory;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.ComboBox cbxCategory;
+        private System.Windows.Forms.GroupBox gbxSearch;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.DataGridView dgwProduct;
+        private System.Windows.Forms.TextBox tbxSearch;
+    }
+}
+
---
+++ RecapProject1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecapProject1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListProducts();
+            ListCategories();
+        }
+
+        private void ListProducts()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {  
+                dgwProduct.DataSource = context.Products.ToList();
+            }
+        }
+
+        private void ListProductsByCategory(int categoryId)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                dgwProduct.DataSource = context.Products.Where(p => p.CategoryId==categoryId).ToList();
+            }
+        }
+
+        private void ListProductsByProductName(string searchKey, int categoryId)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                dgwProduct.DataSource = context.Products.
+                    Where(p => (p.ProductName.ToLower().Contains(searchKey.ToLower())) &&
+                    (p.CategoryId==categoryId)).ToList();
+            }
+        }
+
+        private void ListCategories()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                cbxCategory.DataSource = context.Categories.ToList();
+                cbxCategory.DisplayMember = "CategoryName";
+                cbxCategory.ValueMember = "CategoryId";
+            }
+        }
+
+        private void cbxCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                ListProductsByCategory(Convert.ToInt32(cbxCategory.SelectedValue));
+            }
+            catch (Exception)
+            {
+
+            }
+            
+        }
+
+        private void tbxSearch_TextChanged(object sender, EventArgs e)
+        {
+            string key = tbxSearch.Text;
+            if(string.IsNullOrEmpty(key))
+            {
+                ListProducts();
+            }
+            else
+            {
+                ListProductsByProductName(tbxSearch.Text, Convert.ToInt32(cbxCategory.SelectedValue));
+            }
+        }
+    }
+}
